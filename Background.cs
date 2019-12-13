e027a067b2e13568f0892581ae6c4d7369159b0d
Jun Park <junpark4395@gmail.com>
2019-12-13 18:40:56 +0800

    Add background class

---
+++ Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TabletArtco
+{
+    class Background
+    {
+        public static List<List<Background>> backgrounds { get; set; } = new List<List<Background>>();
+        public static Background currentBack { get; set; } = null;
+
+        public string _name { get; set; }
+        public int _category { get; set; }
+        public string _path { get; set; }
+        public string _coverPath { get; set; }
+        public string _soundPath { get; set; }
+        public int _level { get; set; }
+        public int _mode { get; set; }
+        public int _idx { get; set; }
+        public bool _isPng { get; set; }
+    }
+}
